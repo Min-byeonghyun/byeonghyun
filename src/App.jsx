0e2c793920989494229bc5e93bcd31d2dacf5d3a
Min-byeonghyun <qudgus5125@naver.com>
2024-10-25 19:34:49 +0900

    폰트 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
-import AppRoutes from './routes/AppRoutes'; // 라우팅 설정 파일
+import AppRoutes from './routes/AppRoutes'; 
 
 const GlobalStyles = createGlobalStyle`
 ${reset};
+@import url("https://fonts.googleapis.com/css2?family=Fredoka:wght@300..700&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700&display=swap");
 * {
   box-sizing: border-box;
 }
 
 body {
-  font-family: 'Roboto', sans-serif; 
+  margin: 0; 
+  font-family: "Montserrat", sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+code {
+  font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New", monospace;
 }
 `;
 
